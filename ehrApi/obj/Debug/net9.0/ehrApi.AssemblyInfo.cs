911954c47f9e0a3c2723aafed54e1ed824b18d17
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-20 09:56:52 +0100

    updated assembly info

--- ehrApi/obj/Debug/net9.0/ehrApi.AssemblyInfo.cs
+++ ehrApi/obj/Debug/net9.0/ehrApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ehrApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad5ce232f941fe60c31b3310d97f89b8c9c2f85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc99d1973020bc3e79decef5a663a996b56808d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ehrApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ehrApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
