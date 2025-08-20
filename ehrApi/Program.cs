eeaa7eb39099dc08a0a42b4eb0f34e9a21d8f86d
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-20 19:24:53 +0100

    uncommented registration lines for interfaces

--- ehrApi/Program.cs
+++ ehrApi/Program.cs
@@ -1,9 +1,13 @@
+using ehrApi.Services.Patients;
+using ehrApi.Services.Orders;
+using ehrApi.Services.Tests;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
-    //builder.Services.AddScoped<IPatientServices,PatientServices>(); 
-    //builder.Services.AddScoped<IOrderServices,OrderServices>(); 
-    //builder.Services.AddScoped<ITestServices,TestServices>(); 
+    builder.Services.AddScoped<IPatientService, PatientService>(); // Registering interfaces for DI container
+    builder.Services.AddScoped<IOrderService, OrderService>();
+    builder.Services.AddScoped<ITestService, TestService>();
 }
 
 var app = builder.Build();
